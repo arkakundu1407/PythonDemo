9b968592ae190938b63a823a605a45c24e2f1680
arkakundu1407 <48091505+arkakundu1407@users.noreply.github.com>
2019-03-14 18:14:43 +0530

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,11 @@
 
-
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-   
+   # return HttpResponse('HELLO FROM RUBAI')
     return render(request, 'posts/index.html')
+
+def add (x,y):
+    return(x+y)
